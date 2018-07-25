48ea03a9fb4af2e1b203ac53fe00cdc9e233e7dd
27hao <1119284545@qq.com>
2018-07-25 23:26:59 +0800

    sql reject

--- src/main/java/top/ralphb/crackdemo/controller/CrackController.java
+++ src/main/java/top/ralphb/crackdemo/controller/CrackController.java
@@ -1,15 +1,27 @@
 package top.ralphb.crackdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import top.ralphb.crackdemo.dao.UserDao;
 
 @Controller
 public class CrackController {
-    @RequestMapping("/first")
-    public String first(Model model) {
-        System.out.printf("test");
-        model.addAttribute("message", "Hello World!");
+    @Autowired
+    UserDao userDao;
+    @RequestMapping("/index")
+    public String index(Model model) {
         return "login";
     }
+    @RequestMapping("/login")
+    public String login(@RequestParam("user") String user,@RequestParam("pass") String pass) {
+        if(userDao.login(user,pass)) {
+            return "success";
+        }else {
+            return "fail";
+        }
+    }
 }
+
--- src/main/java/top/ralphb/crackdemo/dao/UserDao.java
+++ src/main/java/top/ralphb/crackdemo/dao/UserDao.java
@@ -1,15 +1,18 @@
 package top.ralphb.crackdemo.dao;
 
+import org.springframework.stereotype.Component;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+@Component
 public class UserDao {
     public boolean login(String user,String pass){
         Connection conn= null;
         try {
             String sql ="select * from user where username='"+user+"' and password='"+pass+"';";
+            System.out.println(sql);
             conn = DBHelper.getConnection();
             PreparedStatement pst = conn.prepareStatement(sql);// 预处理
             ResultSet rs = pst.executeQuery();
@@ -17,7 +20,6 @@ public class UserDao {
                 return true;
             }
         } catch (Exception e) {
-            e.printStackTrace();
         }
 
         return false;
